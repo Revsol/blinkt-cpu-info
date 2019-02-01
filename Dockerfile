bdddb2a0ce326fe32e7f7560a702e254f3faddf4
Christoph Isecke <filou_c@gmx.de>
2019-02-02 00:25:31 +0100

    script and dockerfile added

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM resin/rpi-raspbian:jessie
+
+RUN apt-get update -qy && apt-get install -y \
+    python \
+    python-rpi.gpio
+	
+
+RUN apt-get install -y libraspberrypi-bin
+
+# RUN apt-get install -y gcc python-dev python-pip
+
+# RUN pip install psutil
+
+RUN apt-get install python-blinkt
+
+RUN apt-get install python-psutil
+
+# Cancel out any Entrypoint already set in the base image.
+ENTRYPOINT []	
+
+# WORKDIR /root/
+
+# COPY library	library
+# WORKDIR /root/library
+# RUN python setup.py install
+
+WORKDIR /root/
+COPY scripts	scripts
+WORKDIR /root/scripts
+
+ENV Mode="Mixed"
+
+CMD ["python", "cpu_info.py"]
---
+++ scripts/cpu_info.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import os
+import time
+import psutil
+from subprocess import PIPE, Popen
+import blinkt
+
+mode = os.environ['Mode']
+if mode not in ['Mixed', 'Temp', 'Load']:
+    raise ValueError('Mode not recognized.')
+
+blinkt.set_clear_on_exit()
+blinkt.set_brightness(0.05)
+
+
+def get_cpu_temperature():
+    process = Popen(['vcgencmd', 'measure_temp'], stdout=PIPE)
+    output, _error = process.communicate()
+    output = output.decode()
+
+    pos_start = output.index('=') + 1
+    pos_end = output.rindex("'")
+
+    temp = float(output[pos_start:pos_end])
+
+    return temp
+
+
+def show_graph(temp, load):
+    temp *= blinkt.NUM_PIXELS
+    load *= blinkt.NUM_PIXELS
+    for x in range(blinkt.NUM_PIXELS):
+        r, g, b = 0, 0, 0
+        if temp > 0:
+            r = 255
+        if load > 0:
+            b = 255
+
+        blinkt.set_pixel(x, r, g, b)
+        temp -= 1
+        load -= 1
+
+    blinkt.show()
+
+
+while True:
+    temp = 0
+    load = 0
+    if mode in ['Mixed', 'Temp']:
+        temp = get_cpu_temperature() / 100
+    if mode in ['Mixed', 'Load']:
+        load = psutil.cpu_percent() / 100.0
+    show_graph(temp, load)
+    time.sleep(0.05)
